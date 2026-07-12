a48f3d21e6b6dea7231872e73fa58bb6c8bab81e
Brad Arnett <brad.arnett@gmail.com>
2026-07-12 15:46:57 -0700

    mutate the config god object instead of replacing it so we can keep using it

--- lotek/lib/context.py
+++ lotek/lib/context.py
@@ -1,13 +1,15 @@
 """general purpose context object"""
 
+from types import SimpleNamespace
 from zoneinfo import ZoneInfo
 from lotek.lib.logger import log
 
-config = None
+config = SimpleNamespace()
 pac = None
 highlight_formatter = None
 
 def update_pac(new_pac=None):
+    """Replaces the pac (ZoneInfo) object, making dynamic imports required."""
     global pac  # pylint: disable=global-statement
     if not new_pac:
         pac = ZoneInfo(config.rss.timezone)
@@ -16,8 +18,9 @@ def update_pac(new_pac=None):
 
 
 def update_config(new_config):
+    """modifies config in place so dynamic import is unnecessary"""
     global config  # pylint: disable=global-statement
-    config = new_config
+    config.__dict__.update(new_config.__dict__)
     update_pac(ZoneInfo(config.rss.timezone))
     # Apply log level from config if available
     log_level = getattr(getattr(config, "lotek", {}), "log_level", "info")
@@ -27,5 +30,6 @@ def update_config(new_config):
         log.setLevel(getattr(logging, log_level.upper(), logging.INFO))
 
 def update_formatter(formatter):
+    """Replaces the context object, dynamic import required"""
     global highlight_formatter
     highlight_formatter = formatter
