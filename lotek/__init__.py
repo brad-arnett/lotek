5899ff77a1a53827fcefe9a78f0cad3f1f64594b
Brad Arnett <brad.arnett@gmail.com>
2026-08-15 10:22:17 -0700

    fix import order issue also causing unknown version

--- lotek/__init__.py
+++ lotek/__init__.py
@@ -1,13 +1,14 @@
 """lotek.run - A static site builder."""
 
 from importlib.metadata import version, PackageNotFoundError
-from lotek import cli
-from lotek.lib.build import build
 
 try:
     __version__ = version("lotek")
 except PackageNotFoundError:
     __version__ = "unknown"
+
+from lotek import cli
+from lotek.lib.build import build
 
 
 def main():
